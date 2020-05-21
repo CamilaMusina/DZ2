b02147eb5afa059496c048c294e00fe8cce36120
CamilaMusina <56271888+CamilaMusina@users.noreply.github.com>
2020-05-21 14:43:04 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+//#include "Home.h"
+#include "Street.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Street ulitsa;
+    ulitsa.SetNameStreet("Tverskaya");
+    ulitsa.SetHomeNums(3);
+    ulitsa.SetHomes();
+    cout << "Всего избирателей на улице " << ulitsa.GetNameStreet() << ": ";
+    cout << ulitsa.GetMenNums() << endl;
+    for (auto x : ulitsa.GetVectorHouses())
+    {
+        cout << "Всего избирателей в доме " << x.GetiHome() << ": ";
+        cout << x.GetMenCount() << endl;
+    }
+    auto vec = ulitsa.GetVectorHouses();
+    cout << "Количество этажей в доме №" << vec[1].GetiHome() << ": " << vec[1].GetNumbFloor() << endl;
+    
+    Street road("Bronnaya");
+    road.SetHomeNums(5);
+    auto arr = road.GetVectorHouses();
+    arr[0].SetiHome(16);
+    arr[0].SetNumbFloor(3);
+    arr[0].SetNumbApartament(15);
+    arr[0].SetMenCount(47);
+    arr[2] = arr[0];
+    cout << arr[0] << arr[1] << arr[2];
+    return 0;
+}
